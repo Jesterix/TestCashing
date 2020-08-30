e19f71b85e757c7ae22b0306cbb45a509fc9591c
George Khaydenko <ghaidenko@gmail.com>
2020-08-30 17:43:11 +0300

    add AppState for storing app data

---
+++ TestCashing/AppState.swift
@@ -0,0 +1,14 @@
+//
+//  AppState.swift
+//  TestCashing
+//
+//  Created by Georgy Khaydenko on 30.08.2020.
+//  Copyright © 2020 Georgy Khaydenko. All rights reserved.
+//
+
+import Foundation
+
+struct AppState: Codable {
+    var screen: Int
+    var text: String
+}
