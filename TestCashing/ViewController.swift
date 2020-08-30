937e08b63da68c8ae12c165ad9882755ba02a6dc
George Khaydenko <ghaidenko@gmail.com>
2020-08-30 17:24:30 +0300

    initial commit

---
+++ TestCashing/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TestCashing
+//
+//  Created by Georgy Khaydenko on 30.08.2020.
+//  Copyright © 2020 Georgy Khaydenko. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Cashing practice"
+        
+        let button = UIButton(type: .contactAdd)
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
+        
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
